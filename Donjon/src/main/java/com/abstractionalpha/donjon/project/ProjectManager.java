ad17013a6a918417e6e42c6465152bedab639f18
abstractionalpha <abstractionalpha@abstractionAlpha>
2022-12-21 20:37:59 -0500

    [Implementation] Implemented ProjectManager methods

--- Donjon/src/main/java/com/abstractionalpha/donjon/project/ProjectManager.java
+++ Donjon/src/main/java/com/abstractionalpha/donjon/project/ProjectManager.java
@@ -19,13 +19,75 @@ public class ProjectManager {
 		
 	}
 	
-	public void addProject( String name ) {}
+	public void addProject( String name ) throws IllegalArgumentException {
+		
+		if ( getProject( name ) == null ) {
+		
+			projects.add( new Project( name ) );
+			
+		} else {
+			
+			throw new IllegalArgumentException( String.format( "Project %s already exists.", name ) );
+			
+		}
+		
+	}
 	
-	public void addSession( String projectName ) {}
+	public void addSession( String projectName ) throws IllegalArgumentException {
+		
+		Project project = getProject( projectName );
+		if ( project != null ) {
+			
+			Session session = new Session();
+			project.addSession( session );
+			active.add( session );
+			
+		} else {
+			
+			throw new IllegalArgumentException( String.format( "Project %s does not exist.", projectName ) );
+			
+		}
+		
+	}
 	
-	public void addNote( Session session ) {}
+	private Project getProject( String name ) {
+		
+		for ( Project p : projects ) { if ( p.getName().equals( name ) )  return p; }
+		
+		return null;
+		
+	}
 	
-	public void finish( Session session ) {}
+	public void addNote( Session session ) throws IllegalArgumentException {
+		
+		validate( session );
+		
+		session.addNote( note );
+		
+	}
+	
+	public void finish( Session session ) {
+		
+		validate( session );
+		
+		if ( active.contains( session ) ) {
+			
+			active.remove( session );
+			session.finish();
+			
+		} else {
+			
+			throw new IllegalArgumentException( "Session isn't active." );
+			
+		}
+		
+	}
+	
+	private void validate( Session session ) throws IllegalArgumentException {
+		
+		if ( session == null ) { throw new IllegalArgumentException( "Session is null." ); }
+		
+	}
 	
 	// TODO implement later
 	// public Object toXml() { return null; }
